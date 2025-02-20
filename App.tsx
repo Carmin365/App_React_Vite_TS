5d3d120bca49c3c2ed46b0e8e8b16d2c8f9a081e
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2025-02-19 22:25:23 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ComponenteClass from "./ComponenteClass"; 
+
+function App() {
+  return (
+    <div>
+      <h1>Meu App React + Vite!</h1>
+      <p>TypeScript é Formidável!</p>
+      <ComponenteClass />
+    </div>
+  );
+}
+
+export default App;
+
+    
+ 
+
---
+++ ComponenteClass.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+  export default class ComponenteClass extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Componente da Classe</h1>
+          <p>Este é um componente baseado em classe no React com TypeScript.</p>
+        </div>
+      );
+    }
+  }
+  
